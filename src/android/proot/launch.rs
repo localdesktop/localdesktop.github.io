441bf933b46c77794e6fd2676335d137fbbb173d
mister-teddy <hongphat.js@gmail.com>
2026-03-06 22:35:47 +0100

    (trial) fix: function not implemented

--- src/android/proot/launch.rs
+++ src/android/proot/launch.rs
@@ -12,9 +12,46 @@ pub fn launch() {
         let username = local_config.user.username;
 
         let full_launch_command = local_config.command.launch;
+        let run_launch = |saw_execve_enosys: &mut bool| {
+            ArchProcess::exec_as(&full_launch_command, &username).with_log(|it| {
+                if ArchProcess::is_execve_enosys(&it) {
+                    *saw_execve_enosys = true;
+                }
+                log::trace!("{}", it);
+            })
+        };
 
-        ArchProcess::exec_as(&full_launch_command, &username).with_log(|it| {
-            log::trace!("{}", it);
-        });
+        let mut saw_execve_enosys = false;
+        match run_launch(&mut saw_execve_enosys) {
+            Ok(status) if !status.success() => {
+                log::warn!("Desktop launch command exited with status: {:?}", status.code());
+                if saw_execve_enosys && !ArchProcess::no_seccomp_enabled() {
+                    ArchProcess::enable_no_seccomp_fallback("desktop launch execve ENOSYS");
+                    log::warn!(
+                        "Retrying desktop launch with PROOT_NO_SECCOMP=1 after ENOSYS failure"
+                    );
+                    let mut saw_retry_execve_enosys = false;
+                    match run_launch(&mut saw_retry_execve_enosys) {
+                        Ok(retry_status) if !retry_status.success() => {
+                            log::error!(
+                                "Desktop launch retry exited with status: {:?}",
+                                retry_status.code()
+                            );
+                        }
+                        Ok(_) => {}
+                        Err(retry_err) => {
+                            log::error!(
+                                "Failed to run desktop launch retry command: {}",
+                                retry_err
+                            );
+                        }
+                    }
+                }
+            }
+            Ok(_) => {}
+            Err(err) => {
+                log::warn!("Failed to run desktop launch command: {}", err);
+            }
+        }
     });
 }
